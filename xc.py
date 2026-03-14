2b1173e522130092374381ce8898367c183d3e59
Alexander Demin <alexander@demin.ws>
2026-03-14 20:40:25 +0000

    bump version

--- xc.py
+++ xc.py
@@ -27,7 +27,7 @@ from datetime import datetime
 from pathlib import Path
 from typing import Callable
 
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 
 # ---------------------------------------------------------------------------
 # Logging
